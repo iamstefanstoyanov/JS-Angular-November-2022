b3a569a1f835e19ac5ba06ca2ef659427d651c0c
Stefan Stoyanov <96234247+iamstefanstoyanov@users.noreply.github.com>
2022-12-05 20:33:47 +0200

    Add files via upload

---
+++ Workshop Forms/src/app/auth/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  appEmailDomains = appEmailDomains
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private authServie: AuthService) {
+
+  }
+
+  loginHandler(form: NgForm): void {
+    if(form.invalid){return}
+    this.authServie.user = {
+     username: 'John'
+  } as any;
+
+    const returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'] || '/';
+
+    this.router.navigate([returnUrl]);
+  }
+}
---
+++ Workshop Forms/src/app/auth/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailDomains } from 'src/app/shared/constants';
+import { appEmailValidator, sameValueGroupValidator } from 'src/app/shared/validators';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(appEmailDomains)]],
+    ext: [''],
+    tel: [''],
+    pass: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rePassword: []
+    }, {
+      validators: [sameValueGroupValidator('password', 'rePassword')]
+    })
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  registerHandler() {
+    console.log(this.form.value);
+  }
+}
